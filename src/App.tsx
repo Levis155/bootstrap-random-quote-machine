2ce027195c9778005ba28d2bed58fbe028fac1cf
Levis155 <levismbui@gmail.com>
2025-08-13 19:49:05 +0300

    place all required elements on the page

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,25 @@
-import './App.css'
+import { FaSquareXTwitter } from "react-icons/fa6";
+import { FaTumblr } from "react-icons/fa";
 
 function App() {
   return (
-    <div>Hello world!</div>
+    <div className="container-fluid bg-primary min-vh-100 d-flex flex-column justify-content-center align-items-center">
+      <div className="bg-white w-50">
+        <p>Believe you can and you're halfway there.</p>
+        <p>- Theodore Roosevelt</p>
+        <div>
+          <div>
+            <button><FaSquareXTwitter /></button>
+            <button><FaTumblr /></button>
+          </div>
+          <div>
+            <button>new quote</button>
+          </div>
+        </div>
+      </div>
+
+      <span>by Levis</span>
+    </div>
   )
 }
 
